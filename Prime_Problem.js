9ef5a8955d90aa481507e6f744761fdf30ce5b61
unknown <sanjaybaro2017@gmail.com>
2023-02-20 23:09:32 +0530

    here the code writeen have some error please fixed it

---
+++ Prime_Problem.js
@@ -0,0 +1,23 @@
+let number=5;
+
+let count=0;
+
+for(let i=1;i<=number;i++)
+{
+
+if(number%i==0)
+{
+count++;
+
+}
+
+}
+if(count==2){
+
+Print("Prime Number)
+
+}else{
+
+print("Not a prime number)
+
+}
